d240205890f9a032da1fa69a2b789d5d8696c48d
Cameron Hunter <hello@cameronhunter.co.uk>
2012-11-03 15:53:57 +0000

    Pulling inventory functionality into its own callable - it uses the low level AWS api.

---
+++ src/main/java/org/csanchez/aws/glacier/actions/Inventory.java
@@ -0,0 +1,59 @@
+package org.csanchez.aws.glacier.actions;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.concurrent.Callable;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import com.amazonaws.services.glacier.AmazonGlacierClient;
+import com.amazonaws.services.glacier.model.GetJobOutputRequest;
+import com.amazonaws.services.glacier.model.GetJobOutputResult;
+import com.amazonaws.services.glacier.model.InitiateJobRequest;
+import com.amazonaws.services.glacier.model.InitiateJobResult;
+import com.amazonaws.services.glacier.model.JobParameters;
+
+public class Inventory implements Callable<File> {
+
+    private static final Log LOG = LogFactory.getLog( Inventory.class );
+    
+    private final AmazonGlacierClient client;
+    private final String vault;
+
+    public Inventory( AmazonGlacierClient client, String vault ) {
+        this.client = client;
+        this.vault = vault;
+    }
+    
+    public File call() {
+        InputStream in = null;
+        OutputStream out = null;
+        try {
+            LOG.info( "Requesting inventory of vault \"" + vault + "\"" );
+            File temp = File.createTempFile( "glacier-" + vault + "-inventory", ".json" );
+            
+            InitiateJobResult jobRequest = client.initiateJob( new InitiateJobRequest( vault, new JobParameters( "JSON", "inventory-retrieval", null, null ) ) );
+            GetJobOutputResult result = client.getJobOutput( new GetJobOutputRequest( vault, jobRequest.getJobId(), null ) );
+
+            in = result.getBody();
+            out = new FileOutputStream( temp );
+            
+            IOUtils.copy( in, out );
+            
+            out.flush();
+            
+            LOG.info( "Successfully retrieved inventory of vault \"" + vault + "\"" );
+            return temp;
+        } catch ( Exception e ) {
+            throw new RuntimeException( "Failed to retrieve inventory of vault \"" + vault + "\"", e );
+        } finally {
+            IOUtils.closeQuietly( out );
+            IOUtils.closeQuietly( in );
+        }
+    }
+
+}
