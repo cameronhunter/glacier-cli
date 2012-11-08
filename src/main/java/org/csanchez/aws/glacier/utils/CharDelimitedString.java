01910878fb6c28141eaaf6eb8f087cb70f4585e8
Cameron Hunter <hello@cameronhunter.co.uk>
2012-11-08 16:08:07 +0000

    Adding tests for CharDelimitedString

--- src/main/java/org/csanchez/aws/glacier/utils/CharDelimitedString.java
+++ src/main/java/org/csanchez/aws/glacier/utils/CharDelimitedString.java
@@ -1,12 +1,24 @@
 package org.csanchez.aws.glacier.utils;
 
+import static com.google.common.collect.Iterables.transform;
+import static java.util.Arrays.asList;
+import static org.apache.commons.lang.StringUtils.EMPTY;
+
+import com.google.common.base.Function;
 import com.google.common.base.Joiner;
 
 public final class CharDelimitedString {
 
+    private static final Function<Object, Object> NULL_TO_EMPTY_STRING = new Function<Object, Object>() {
+        @Override
+        public Object apply( Object input ) {
+            return input == null ? EMPTY : input;
+        }
+    };
+
     public static String tsv( Object... fields ) {
-        return Joiner.on( '\t' ).join( fields );
+        return Joiner.on( '\t' ).join( transform( asList( fields ), NULL_TO_EMPTY_STRING ) );
     }
-    
+
     private CharDelimitedString() {}
 }
